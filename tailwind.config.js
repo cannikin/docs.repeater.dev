1906592b279570b36a2c4354a58bdd4fe645e795
Rob Cameron <rob.cameron@fastmail.com>
2020-08-20 10:42:40 -0700

    More docs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 /* See https://tailwindcss.com/docs/configuration for more options */
 
 module.exports = {
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'sans-serif'],
+        mono: ['Fira Code', 'Fira Mono', 'Menlo', 'Monoco', 'monospace'],
+      },
+    }
+  },
   variants: {},
   plugins: []
 };
